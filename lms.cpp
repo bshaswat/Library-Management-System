41b68a1a17d23c6ca9a6b78e8eefc6462c8f1222
Shaswat <bhattaraishaswat@gmail.com>
2020-07-09 21:58:09 -0600

    initial

---
+++ lms.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+class Book {
+	private char name[25];
+  private int bookNo;
+  private char location[10];
+  private bool checkedOut;
+  
+  public Book(char[] n, int no, char[] loc, book check) {
+  	name = n;
+    bookNo = no;
+    location = loc;
+    checkedOut = check;
+  }
+  
+  public char[] getName() {
+		return name;
+  }
+  
+  public int getBookNo() {
+    return bookNo;
+  }
+  
+  public char[] getLocation() {
+  	return location;
+  }
+  
+  public void setName(char[] inputName) {
+    name = inputName;
+  }
+  
+  public void setBookNo(int inputBookNo) {
+    bookNo = inputBookNo;
+  }
+  
+  public void setLocation(char[] inputLocation) {
+    location = inputLocation;
+  }
+  
+   public bool getCheckedOut() {
+  	return checkedOut;
+  }
+  
+  public void setCheckedOut(bool checked) {
+    checkedOut = checked;
+  }
+}
+
+class Student {
+  private char name[25];
+  private int studentNo;
+  private char address[25];
+  
+  
+}
+
+int menu() {
+  int choice;
+  cout << "Library";
+  cout << "1. Check out a book." << "\n2. Return a book." << "\n3.Get a library card." << "4. Pay Fines";
+  cout << "\nWhat do you want to do? Enter option (1-4): ";
+  cin >> choice;
+  return choice;
+}
+
+void checkBook() {
+}
+
+void returnBook() {
+}
+
+void newCard() {
+}
+
+void payfine() {
+}
+
+int main() {
+  int choice = menu();]
+  Student student1;
+  if (choice = 1) {
+    checkBook();
+  }
+  else if (choice = 2) {
+    returnBook();
+  }
+  else if (choice = 3) {
+    newCard();
+  }
+  else if (choice = 4) {
+    payFine();
+  }
+  else {
+    cout << "Enter a valid choice.";
+  }
+}
